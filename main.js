638152a3733699d758993ea243c95a5aae9895cb
Talib Raeen <talibraeen13@gmail.com>
2024-04-22 17:45:18 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+
+setInterval(() => {
+
+  let vaqt = new Date();
+
+  let comTime = vaqt.getHours() + ":" + vaqt.getMinutes() + ":" + vaqt.getSeconds();
+
+  // Dates and days
+  let tareekh = vaqt.toLocaleDateString();
+
+
+  document.getElementById(`tym`).innerHTML = comTime;
+  document.getElementById(`date`).innerHTML = tareekh;
+}, 1);
+
+
+// * Adam bg img
